dee61f79c1f3a0cc298b49f47a11eafbf9349d8e
derekjpeters <derek.j.peters@gmail.com>
2024-11-14 16:51:21 -0500

    updated the HTML for today's lesson

--- script.js
+++ script.js
@@ -5,40 +5,48 @@ header.style.color = 'blue';
 
 const paragraphs = document.getElementsByClassName('text');
 console.log(paragraphs);
-// console.log(paragraphs.length);
-// //Loop through the collection of paragraphs
-// for (let i = 0; i < paragraphs.length; i++) {
-//     paragraphs[i].style.color = '#F28500';
-//     paragraphs[i].style.backgroundColor = 'aqua';
-// }
+console.log(paragraphs.length);
+//Loop through the collection of paragraphs
+for (let i = 0; i < paragraphs.length; i++) {
+    paragraphs[i].style.color = '#F28500';
+    paragraphs[i].style.backgroundColor = 'aqua';
+}
 
-// const colorButton = document.querySelector('#change-color-btn');
-// console.log(colorButton);
+const colorButton = document.querySelector('#change-color-btn');
+console.log(colorButton);
 
-// colorButton.addEventListener('click', () => {
-//     const allText = document.querySelectorAll('.text');
+colorButton.addEventListener('click', () => {
+    const allText = document.querySelectorAll('.text');
 
-//     //Loop over NodeList of paragraphs
-//     allText.forEach(paragraph => {
-//         paragraph.style.backgroundColor = '#5d6c89'
-//     });
-// });
+    //Loop over NodeList of paragraphs
+    allText.forEach(paragraph => {
+        paragraph.style.backgroundColor = '#5d6c89'
+    });
+});
 
-// const firstParagraph = document.querySelector('p');
-// console.log(firstParagraph);
+const button = document.getElementById('change-color-btn');
 
-// firstParagraph.style.fontSize = '20px';
-// firstParagraph.style.color = '#fbf6f3';
+function handleClick() {
+    console.log('Button was Clicked')
+}
 
-// const allParagraphs = document.querySelectorAll('.text')
-// console.log(allParagraphs)
+button.addEventListener('click', handleClick);
 
-// allParagraphs.forEach((paragraph, index) => {
-//     paragraph.style.margin = '10px 0';
-//     paragraph.style.color = 'black';
-//     paragraph.style.fontSize = '15px';
-//     console.log(`Paragraph ${index + 1}`, paragraph);
-// })
+const firstParagraph = document.querySelector('p');
+console.log(firstParagraph);
+
+firstParagraph.style.fontSize = '20px';
+firstParagraph.style.color = '#fbf6f3';
+
+const allParagraphs = document.querySelectorAll('.text')
+console.log(allParagraphs)
+
+allParagraphs.forEach((paragraph, index) => {
+    paragraph.style.margin = '10px 0';
+    paragraph.style.color = 'black';
+    paragraph.style.fontSize = '15px';
+    console.log(`Paragraph ${index + 1}`, paragraph);
+})
 
 const changeHeader = document.getElementById('main-header');
 changeHeader.textContent = 'DOM Manipulation Updated Lesson';
@@ -55,6 +63,12 @@ colorButtonTwo.setAttribute('title', 'Click to change colors');
 const paragraphSecOne = document.getElementsByClassName('text');
 console.log(paragraphSecOne.length);
 
+document.addEventListener('keydown', (event) => {
+    if(event.key === 'J' || event.key === 'j') {
+        console.log('The "J" key was pressed')
+    }
+});
+
 for (let i = 0; i < paragraphSecOne.length; i++) {
     //Change class name using .className
     console.log(paragraphSecOne[i].className);
@@ -64,7 +78,7 @@ for (let i = 0; i < paragraphSecOne.length; i++) {
     paragraphSecOne[i].classList.add('highlight');
 }
 
-//console.log(paragraphSecOne);
+console.log(paragraphSecOne);
 
 
 
